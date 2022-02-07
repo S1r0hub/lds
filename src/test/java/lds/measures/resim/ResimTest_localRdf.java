59c8702b21c5ebabda6bba195932f9769c5fa5d1
S1r0hub <18535730+S1r0hub@users.noreply.github.com>
2022-02-07 17:19:07 +0100

    fixed local Resim Test and verified values of paper example (correct afaik)

--- src/test/java/lds/measures/resim/ResimTest_localRdf.java
+++ src/test/java/lds/measures/resim/ResimTest_localRdf.java
@@ -16,7 +16,6 @@ import ldq.LdDataset;
 import ldq.LdDatasetFactory;
 
 /**
- *
  * @author Fouad Komeiha
  */
 public class ResimTest_localRdf {
@@ -26,66 +25,41 @@ public class ResimTest_localRdf {
 	@Test
 	public void isResimWorksCorrectlyOnPaperExample() throws Exception{
 
-
 		LdDataset dataSet = null;
 
-		R r1 = new R("http://www.example.org#Fish");
-		R r2 = new R("http://www.example.org#Whale");
-
 		try {
-			dataSet = LdDatasetFactory.getInstance().name("example").file(dataSetDir)
-					.defaultGraph("http://graphResim/dataset").create();
+			// FIX by S1r0hub: the following caused the test to fail, as there is no default graph name in the data!
+			dataSet = LdDatasetFactory.getInstance()
+				.name("example").file(dataSetDir)
+				//.defaultGraph("http://graphResim/dataset")
+				.defaultGraph(null) // FIXED
+				.create();
 
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
 
 		Config config = new Config();
-                config.addParam(ConfigParam.useIndexes, false);
-                config.addParam(ConfigParam.LdDatasetMain , dataSet);
+		config.addParam(ConfigParam.useIndexes, false);
+		config.addParam(ConfigParam.LdDatasetMain , dataSet);
 
 		ResourceSimilarity resim = new Resim(config);
-                
-                resim.loadIndexes();
-                
-                double cii = 0, cio = 0, cii_r1 = 0, cio_r1 = 0, cii_r2 = 0, cio_r2 = 0, cii_norm = 0, cio_norm = 0, pptySim,
-				ldsd, ldsdsim, sim;
-                
-		/*Set<URI> edges = resimLdManager.getEdges(r1, r2);
+		resim.loadIndexes();
 
-		
+		double pptySim, ldsdsim, sim;
 
-		for (URI edge : edges) {
-			cii = cii + resim.Cii(edge, r1, r2);
-                        cio = cio + resim.Cio(edge, r1, r2);
-                        cii_r1 = cii_r1 + resim.Cii(edge, r1);
-                        cio_r1 = cio_r1 + resim.Cio(edge, r1);
-                        cii_r2 = cii_r2 + resim.Cii(edge, r2);
-                        cio_r2 = cio_r2 + resim.Cio(edge, r2);
-                        cio_norm = cio_norm + resim.Cio_normalized(edge, r1, r2);
-                        cii_norm = cii_norm + resim.Cii_normalized(edge, r1, r2);
+		R r1 = new R("http://www.example.org#Fish");
+		R r2 = new R("http://www.example.org#Whale");
 
-
-		}
-
-		// components of LDSD in Resim
-		assertEquals(0.0, cii, 0.0);
-		assertEquals(1.0, cio, 0.0);
-		assertEquals(0.0, cii_r1, 0.0);
-		assertEquals(2.0, cio_r1, 0.0);
-		assertEquals(0.0, cii_r2, 0.0);
-		assertEquals(3.0, cio_r2, 0.0);
-		assertEquals(0.0, cii_norm, 0.0);
-		assertEquals(1.0, cio_norm, 0.0);*/
-
+		// result = (7/60 + 1.5) / 3 = 0.5388...
 		sim = resim.compare(r1, r2 , 1, 2);
 		assertEquals(0.5388888888888889, sim, 0.0);
 
+		// result = 7/60 = 0.1166...
 		pptySim = resim.PropertySim(r1, r2);
 		assertEquals(0.11666666666666665, pptySim, 0.0);
 
-
-                ldsdsim = resim.LDSDsim(r1, r2);
+		ldsdsim = resim.LDSDsim(r1, r2);
 		assertEquals(0.75, ldsdsim, 0.0);
 
 		sim = resim.compare(r2, r1 , 1 , 2);
@@ -95,7 +69,5 @@ public class ResimTest_localRdf {
 		assertEquals(1.0, sim, 0.0);
 
 		resim.closeIndexes();
-
 	}
-
 }
