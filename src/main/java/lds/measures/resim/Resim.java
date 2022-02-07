2d54f1349f3635265cfcfa71fed2fecb1aaa69b4
S1r0hub <18535730+S1r0hub@users.noreply.github.com>
2022-02-07 15:23:09 +0100

    class description added and reformatting

--- src/main/java/lds/measures/resim/Resim.java
+++ src/main/java/lds/measures/resim/Resim.java
@@ -10,130 +10,125 @@ import lds.resource.R;
 import org.openrdf.model.URI;
 
 /**
- *
+ * This class is part of the Resource Similarity measure.<br/>
+ * It implements normalized distance measures.
+ * 
  * @author Fouad Komeiha
  */
-
-  public class Resim extends ResourceSimilarity {  
+public class Resim extends ResourceSimilarity {
 
 	public Resim(Config config) throws Exception {
-            super(config);
+		super(config);
 	}
 
-
-        @Override
+	@Override
 	public double LDSD(R a, R b) {
-                double cdA_norm = 0, cdB_norm = 0, cii_norm = 0, cio_norm = 0;
+
+		double cdA_norm = 0, cdB_norm = 0, cii_norm = 0, cio_norm = 0;
 
 		for (URI l : edges) {
-                    
+
 			cdA_norm = cdA_norm + Cd_normalized(l, a, b);
 			cdB_norm = cdB_norm + Cd_normalized(l, b, a);
-			cii_norm = cii_norm + Cii_normalized(l, a , b);
-			cio_norm = cio_norm + Cio_normalized(l, a , b);
+			cii_norm = cii_norm + Cii_normalized(l, a, b);
+			cio_norm = cio_norm + Cio_normalized(l, a, b);
 
-		}      	
-                
+		}
+
 		return 1 / (1 + cdA_norm + cdB_norm + cii_norm + cio_norm);
 	}
 
-
-        @Override
+	@Override
 	public double Cd_normalized(URI l, R a, R b) {
-		int cd = Cd(l, a, b) , cd_l = 0;
+		
+		int cd = Cd(l, a, b), cd_l = 0;
 		double cd_norm = 0;
-                
-                if(cd != 0){
-                    cd_l = Cd(l, a);
-                    if(cd_l != 0){
-                        double x = 1 + Math.log10(cd_l);
-                        cd_norm = (double) cd / x;
-                    }
-                    else
-                      cd_norm = (double) cd;  
-                }
+
+		if (cd != 0) {
+			cd_l = Cd(l, a);
+			if (cd_l != 0) {
+				double x = 1 + Math.log10(cd_l);
+				cd_norm = (double) cd / x;
+			} else
+				cd_norm = (double) cd;
+		}
 
 		return cd_norm;
-
 	}
 
-        @Override
+	@Override
 	public double Cii_normalized(URI l, R a, R b) {
 
 		int ciiA, ciiB, cii;
 		double x, cii_norm = 0;
 
 		cii = Cii(l, a, b);
-                
-                if(cii != 0){
-                    ciiA = Cii(l, a);
-                    ciiB = Cii(l, b);
-                    
-                    if( (ciiA + ciiB) != 0){
-                        x = 1 + Math.log10( (ciiA + ciiB) / 2 );
-                        cii_norm = ((double) cii ) / x;
-                    }
-                    else
-                        cii_norm = (double) cii;
-                }
+
+		if (cii != 0) {
+			ciiA = Cii(l, a);
+			ciiB = Cii(l, b);
+
+			if ((ciiA + ciiB) != 0) {
+				x = 1 + Math.log10((ciiA + ciiB) / 2);
+				cii_norm = ((double) cii) / x;
+			} else
+				cii_norm = (double) cii;
+		}
 
 		return cii_norm;
-
 	}
 
-        @Override
+	@Override
 	public double Cio_normalized(URI l, R a, R b) {
-            int cioA, cioB, cio;
-            double cio_norm = 0, x;
+		
+		int cioA, cioB, cio;
+		double cio_norm = 0, x;
 
-            cio = Cio(l, a, b);
+		cio = Cio(l, a, b);
 
-            if(cio != 0)
-            {
-                cioA = Cio(l, a);
-                cioB = Cio(l, b);
-                
-                if((cioA + cioB) != 0 ){
-                    x = 1 + Math.log10( (cioA + cioB) / 2 );
+		if (cio != 0) {
+			cioA = Cio(l, a);
+			cioB = Cio(l, b);
 
-                    cio_norm = ((double) cio ) / x;
-                }
-                else
-                    cio_norm = (double) cio ;
-            }  
+			if ((cioA + cioB) != 0) {
+				x = 1 + Math.log10((cioA + cioB) / 2);
 
-            return cio_norm;
+				cio_norm = ((double) cio) / x;
+			} else
+				cio_norm = (double) cio;
+		}
 
+		return cio_norm;
 	}
 
-    @Override
-    public int Cii(URI li, URI lj, R a, R b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public int Cii(URI li, URI lj, R a, R b) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public int Cii(URI li, URI lj , R k) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public int Cii(URI li, URI lj, R k) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public double Cii_normalized(URI li, URI lj, R a, R b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public double Cii_normalized(URI li, URI lj, R a, R b) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public int Cio(URI li, URI lj, R a, R b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public int Cio(URI li, URI lj, R a, R b) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public int Cio(URI li, URI lj , R k) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public int Cio(URI li, URI lj, R k) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
-    @Override
-    public double Cio_normalized(URI li, URI lj, R a, R b) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+	@Override
+	public double Cio_normalized(URI li, URI lj, R a, R b) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
 
 }
--- src/main/java/lds/measures/resim/ResourceSimilarity.java
+++ src/main/java/lds/measures/resim/ResourceSimilarity.java
@@ -20,257 +20,233 @@ import lds.measures.LdSimilarity;
 import lds.measures.weight.Weight;
 
 /**
- *
+ * Main class for
+ * <a href="https://doi.org/10.1007/978-3-319-31676-5_13">Resource Similarity (Resim) by Piao et al. 2015</a>.
+ * 
  * @author Fouad Komeiha
  */
 public abstract class ResourceSimilarity implements LdSimilarity {
-    protected List<URI> edges;
-    protected ResimLdManager resimLDLoader;
-    protected ResimLdManager SpecificResimLdLoader;
-    protected Weight weight;
-    protected boolean useIndeses;
-    
-    public ResourceSimilarity(Config config) throws Exception{
-        int confsize = config.getSize();
-        switch(confsize){
-            case 0:
-                throw new Exception("Configuration parameters missing"); 
-                
-            case 1:
-                throw new Exception("Some configuration parameters missing");  
-                
-            case 2:
-                this.resimLDLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetMain) , (Boolean) config.getParam(ConfigParam.useIndexes) );
-                this.useIndeses = (Boolean) config.getParam(ConfigParam.useIndexes);
-                break;
-                
-            case 3:
-                throw new Exception("Some configuration parameters missing");
-                
-            default:
-                this.resimLDLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetMain) , (Boolean) config.getParam(ConfigParam.useIndexes) );
-                this.SpecificResimLdLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetSpecific) , (Boolean) config.getParam(ConfigParam.useIndexes) );
-                this.weight = new Weight((WeightMethod)config.getParam(ConfigParam.WeightMethod) , resimLDLoader , SpecificResimLdLoader , (Boolean)config.getParam(ConfigParam.useIndexes));
-                this.useIndeses = (Boolean) config.getParam(ConfigParam.useIndexes);
-                break;
-        }
-    }
-    
-    
-    @Override
-    public void closeIndexes(){
-        if(useIndeses){
-            resimLDLoader.closeIndexes();
-            
-            if(SpecificResimLdLoader != null && weight != null){
-                SpecificResimLdLoader.closeIndexes();
-                weight.closeIndexes();
-            }
-        }
-        
-        //close prefixes and namespaces index
-        Ontology.closeIndexes();
-    }
-    
-    @Override
-    public void loadIndexes() throws Exception{
+	protected List<URI> edges;
+	protected ResimLdManager resimLDLoader;
+	protected ResimLdManager SpecificResimLdLoader;
+	protected Weight weight;
+	protected boolean useIndeses;
 
-        if(useIndeses){
-            resimLDLoader.loadIndexes();
-            
-            if(SpecificResimLdLoader != null && weight != null){
-                SpecificResimLdLoader.loadIndexes();
-                weight.loadIndexes();
-            }
-        }
-        
-        //load prefixes and namespaces index
-        Ontology.loadIndexes();
-    }
+	public ResourceSimilarity(Config config) throws Exception {
 
-    @Override
-    public double compare(R a, R b) {
-               
-        double sim = 0;
-        try {
-            sim = Resim(a, b);
-        } catch (Exception ex) {
-            Logger.getLogger(Resim.class.getName()).log(Level.SEVERE, null, ex);
-            return -1;
-        }
-        
-        return sim;
-    }
-    
-    
-    public double compare(R a, R b , int w1 , int w2) {
-        
-        double sim = 0;
-        try {
-            sim = Resim(a, b , w1 , w2);
-        } catch (Exception ex) {
-            Logger.getLogger(Resim.class.getName()).log(Level.SEVERE, null, ex);
-            return -1;
-        }
-        
-        return sim;
-    }
+		int confsize = config.getSize();
 
-    /**
-     * Retrieve resource similarity of a and b, using weights w1 and w2.
-     * @param a first resource
-     * @param b second resource
-     * @param w1 first weight for property similarity
-     * @param w2 second weight for LDSD similarity
-     * @return Average of the weighted sum of property similarity and LDSD similarity
-     */
-    public double Resim(R a, R b, int w1, int w2) {
+		switch (confsize) {
 
-        // FIX by S1r0hub: if resources are the same, similarity is 1, no matter how many edges there are.
-        // Placing this statement here can potentially save us some requests.
-        if (a.equals(b) || resimLDLoader.isSameAs(a, b)) { return 1; }
+			case 0:
+				throw new Exception("Configuration parameters missing");
 
-        this.edges = resimLDLoader.getEdges(a, b);
-        if(edges == null) { return 0; }
+			case 1:
+				throw new Exception("Some configuration parameters missing");
 
-        System.out.println("Get similarity of a=" + a.toString() + " and b=" + b.toString());
-        double x = w1 * PropertySim(a, b);
-        double y = w2 * LDSDsim(a, b);
-        return (x + y) / (w1 + w2);
-    } 
+			case 2:
+				this.resimLDLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetMain),
+						(Boolean) config.getParam(ConfigParam.useIndexes));
+				this.useIndeses = (Boolean) config.getParam(ConfigParam.useIndexes);
+				break;
+	
+			case 3:
+				throw new Exception("Some configuration parameters missing");
+	
+			default:
+				this.resimLDLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetMain),
+						(Boolean) config.getParam(ConfigParam.useIndexes));
+				this.SpecificResimLdLoader = new ResimLdManager((LdDataset) config.getParam(ConfigParam.LdDatasetSpecific),
+						(Boolean) config.getParam(ConfigParam.useIndexes));
+				this.weight = new Weight((WeightMethod) config.getParam(ConfigParam.WeightMethod), resimLDLoader,
+						SpecificResimLdLoader, (Boolean) config.getParam(ConfigParam.useIndexes));
+				this.useIndeses = (Boolean) config.getParam(ConfigParam.useIndexes);
+				break;
+		}
+	}
 
-    public double Resim(R a, R b) {
-        // FIX by S1r0hub: we only need one constructor and this one calls the other (easier to maintain)
-        return Resim(a, b, 1, 1);
-    }
+	@Override
+	public void closeIndexes() {
+		if (useIndeses) {
+			resimLDLoader.closeIndexes();
 
+			if (SpecificResimLdLoader != null && weight != null) {
+				SpecificResimLdLoader.closeIndexes();
+				weight.closeIndexes();
+			}
+		}
 
-    public abstract double LDSD(R a , R b);
+		// close prefixes and namespaces index
+		Ontology.closeIndexes();
+	}
 
+	@Override
+	public void loadIndexes() throws Exception {
 
-    public double LDSDsim(R a, R b) {
-        return 1 - LDSD(a, b);
-    }
+		if (useIndeses) {
+			resimLDLoader.loadIndexes();
 
-    public double PropertySim(R a, R b) {
+			if (SpecificResimLdLoader != null && weight != null) {
+				SpecificResimLdLoader.loadIndexes();
+				weight.loadIndexes();
+			}
+		}
 
-        double x = 0, y = 0;
+		// load prefixes and namespaces index
+		Ontology.loadIndexes();
+	}
 
-        for (URI e : edges) {
-            int cd = Cd(e);
-            x = x + ((double) Csip(e, a, b) / cd);
-            y = y + ((double) Csop(e, a, b) / cd);
-        }
-        
-        double cip = 0, cop = 0, ip = 0, op = 0;
-        if (x !=0 ) { cip = Cip(a) + Cip(b); }
-        if (y != 0) { cop = Cop(a) + Cop(b); }
-        if (cip != 0) { ip = x / cip; }
-        if (cop != 0) { op = y / cop; }
-        return ip + op;
-    }
+	@Override
+	public double compare(R a, R b) {
 
-    public int Cip(R a) {
-        return resimLDLoader.countSubject(a);
-    }
+		double sim = 0;
+		try {
+			sim = Resim(a, b);
+		} catch (Exception ex) {
+			Logger.getLogger(Resim.class.getName()).log(Level.SEVERE, null, ex);
+			return -1;
+		}
 
-    public int Cop(R a) {
-        return resimLDLoader.countObject(a);
-    }
+		return sim;
+	}
 
-    public int Csip(URI l, R a, R b) {
-        if ( resimLDLoader.countSubject(l , a) > 0 && resimLDLoader.countSubject(l , b ) > 0 )
-            return 1;
-        return 0;
-    }
+	public double compare(R a, R b, int w1, int w2) {
 
-    public int Csop(URI l, R a, R b) {
-        if (resimLDLoader.countObject(l , a) > 0 && resimLDLoader.countObject(l , b ) > 0)
-            return 1;
-        return 0;
-    }
+		double sim = 0;
+		try {
+			sim = Resim(a, b, w1, w2);
+		} catch (Exception ex) {
+			Logger.getLogger(Resim.class.getName()).log(Level.SEVERE, null, ex);
+			return -1;
+		}
 
-    public int Cd(URI l, R a, R b) {
-        if(resimLDLoader.isDirectlyConnected(l, a, b))
-            return 1;
-        else
-            return 0;
-    }
+		return sim;
+	}
 
-    public int Cd(URI l, R a) {
-        return resimLDLoader.countObject(l , a);
-    }
+	/**
+	 * Retrieve resource similarity of a and b, using weights w1 and w2.
+	 * 
+	 * @param a  first resource
+	 * @param b  second resource
+	 * @param w1 first weight for property similarity
+	 * @param w2 second weight for LDSD similarity
+	 * @return Average of the weighted sum of property similarity and LDSD similarity
+	 */
+	public double Resim(R a, R b, int w1, int w2) {
 
-    public int Cd(URI l) {
-        return resimLDLoader.countPropertyOccurrence(l);
-    }
+		// FIX by S1r0hub: if resources are the same, similarity is 1, no matter how
+		// many edges there are.
+		// Placing this statement here can potentially save us some requests.
+		if (a.equals(b) || resimLDLoader.isSameAs(a, b)) { return 1; }
 
-    public abstract double Cd_normalized(URI l, R a, R b);
+		this.edges = resimLDLoader.getEdges(a, b);
+		if (edges == null) { return 0; }
 
-    public int Cii(URI l, R a, R b) {
-        return resimLDLoader.countShareCommonSubjects(l, a , b);
+		System.out.println("Get similarity of a=" + a.toString() + " and b=" + b.toString());
+		double x = w1 * PropertySim(a, b);
+		double y = w2 * LDSDsim(a, b);
+		return (x + y) / (w1 + w2);
+	}
 
-    }
+	public double Resim(R a, R b) {
+		// FIX by S1r0hub: we only need one constructor and this one calls the other
+		// (easier to maintain)
+		return Resim(a, b, 1, 1);
+	}
 
-    public int Cii(URI l, R a) {
-        return resimLDLoader.countShareCommonSubjects(l, a);
-    }
+	public abstract double LDSD(R a, R b);
 
-    public abstract double Cii_normalized(URI l, R a, R b);
+	public double LDSDsim(R a, R b) {
+		return 1 - LDSD(a, b);
+	}
 
-    public int Cio(URI l, R a, R b) {
-        return resimLDLoader.countShareCommonObjects(l, a , b);
+	public double PropertySim(R a, R b) {
 
-    }
+		double x = 0, y = 0;
 
-    public int Cio(URI l, R a) {
-        return resimLDLoader.countShareCommonObjects(l, a);
-    }
+		for (URI e : edges) {
+			int cd = Cd(e);
+			x = x + ((double) Csip(e, a, b) / cd);
+			y = y + ((double) Csop(e, a, b) / cd);
+		}
 
-    public abstract double Cio_normalized(URI l, R a, R b);
+		double cip = 0, cop = 0, ip = 0, op = 0;
+		if (x != 0) { cip = Cip(a) + Cip(b); }
+		if (y != 0) { cop = Cop(a) + Cop(b); }
+		if (cip != 0) { ip = x / cip; }
+		if (cop != 0) { op = y / cop; }
+		return ip + op;
+	}
 
-    public abstract int Cii(URI li , URI lj , R a, R b);
-    
-    public abstract int Cii(URI li , URI lj , R k);
-    
-    public abstract double Cii_normalized(URI li , URI lj , R a, R b);
-    
-    public abstract int Cio(URI li , URI lj , R a, R b);
-    
-    public abstract int Cio(URI li , URI lj , R k);
-    
-    public abstract double Cio_normalized(URI li , URI lj , R a, R b);
-    
-    
-    /*public ResimLdManager getMainLdManager(){
-        return this.resimLDLoader;
-    }
-    
-    public ResimLdManager getSpecificLdManager(){
-        return this.SpecificResimLdLoader;
-    }
-    
-    public Weight getWeight(){
-        return this.weight;
-    }
-    
-    
-    public void setMainLdManager(ResimLdManager manager){
-        this.resimLDLoader = manager;
-    }
-    
-    public void setSpecificLdManager(ResimLdManager manager){
-        this.SpecificResimLdLoader = manager;
-    }
-    
-    public void setWeight(Weight weight){
-        this.weight = weight;
-    }*/
-    
-    @Override
-    public LdSimilarity getMeasure(){
-        return this;
-    }
-    
-    
+	public int Cip(R a) {
+		return resimLDLoader.countSubject(a);
+	}
+
+	public int Cop(R a) {
+		return resimLDLoader.countObject(a);
+	}
+
+	public int Csip(URI l, R a, R b) {
+		if (resimLDLoader.countSubject(l, a) > 0 && resimLDLoader.countSubject(l, b) > 0)
+			return 1;
+		return 0;
+	}
+
+	public int Csop(URI l, R a, R b) {
+		if (resimLDLoader.countObject(l, a) > 0 && resimLDLoader.countObject(l, b) > 0)
+			return 1;
+		return 0;
+	}
+
+	public int Cd(URI l, R a, R b) {
+		if (resimLDLoader.isDirectlyConnected(l, a, b)) { return 1; }
+		return 0;
+	}
+
+	public int Cd(URI l, R a) {
+		return resimLDLoader.countObject(l, a);
+	}
+
+	public int Cd(URI l) {
+		return resimLDLoader.countPropertyOccurrence(l);
+	}
+
+	public abstract double Cd_normalized(URI l, R a, R b);
+
+	public int Cii(URI l, R a, R b) {
+		return resimLDLoader.countShareCommonSubjects(l, a, b);
+	}
+
+	public int Cii(URI l, R a) {
+		return resimLDLoader.countShareCommonSubjects(l, a);
+	}
+
+	public abstract double Cii_normalized(URI l, R a, R b);
+
+	public int Cio(URI l, R a, R b) {
+		return resimLDLoader.countShareCommonObjects(l, a, b);
+	}
+
+	public int Cio(URI l, R a) {
+		return resimLDLoader.countShareCommonObjects(l, a);
+	}
+
+	public abstract double Cio_normalized(URI l, R a, R b);
+
+	public abstract int Cii(URI li, URI lj, R a, R b);
+
+	public abstract int Cii(URI li, URI lj, R k);
+
+	public abstract double Cii_normalized(URI li, URI lj, R a, R b);
+
+	public abstract int Cio(URI li, URI lj, R a, R b);
+
+	public abstract int Cio(URI li, URI lj, R k);
+
+	public abstract double Cio_normalized(URI li, URI lj, R a, R b);
+
+	@Override
+	public LdSimilarity getMeasure() {
+		return this;
+	}
 }
