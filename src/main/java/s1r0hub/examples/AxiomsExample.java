29659a8ed669d13fcd8dc0be4dd814cf718bb204
S1r0hub <18535730+S1r0hub@users.noreply.github.com>
2022-03-04 15:45:03 +0100

    added sim(b,b) example and updated result comment

--- src/main/java/s1r0hub/examples/AxiomsExample.java
+++ src/main/java/s1r0hub/examples/AxiomsExample.java
@@ -34,9 +34,12 @@ public class AxiomsExample {
 
 		// define entities
 		String obo = "http://purl.obolibrary.org/obo/";
-		EntityPair aa = new EntityPair(new R(obo + "FOODON_03411245"), new R(obo + "FOODON_03411245")); // apple tree & apple tree
-		EntityPair ab = new EntityPair(new R(obo + "FOODON_03411245"), new R(obo + "FOODON_03414363")); // apple tree & kiwi
-		EntityPair ba = new EntityPair(new R(obo + "FOODON_03414363"), new R(obo + "FOODON_03411245")); // kiwi & apple tree
+		R a = new R(obo + "FOODON_03411245");
+		R b = new R(obo + "FOODON_03414363");
+		EntityPair aa = new EntityPair(a, a); // apple tree & apple tree
+		EntityPair ab = new EntityPair(a, b); // apple tree & kiwi
+		EntityPair ba = new EntityPair(b, a); // kiwi & apple tree
+		EntityPair bb = new EntityPair(b, b); // kiwi & kiwi
 
 		// Show reliability of results based on axioms (see Resim paper):
 		// 1) sim(a,a) = 1 and sim(a,a) = sim(b,b) => equal self-similarity
@@ -49,19 +52,25 @@ public class AxiomsExample {
 		sim_LDSD = engine_ldsd.similarity(aa.first(), aa.second());
 		sim_Resim = engine_resim.similarity(aa.first(), aa.second());
 		System.out.println(String.format("sim(a,a): LDSD=%f, Resim=%f", sim_LDSD, sim_Resim));
-
+		
+		sim_LDSD = engine_ldsd.similarity(bb.first(), bb.second());
+		sim_Resim = engine_resim.similarity(bb.first(), bb.second());
+		System.out.println(String.format("sim(b,b): LDSD=%f, Resim=%f", sim_LDSD, sim_Resim));
+		
 		sim_LDSD = engine_ldsd.similarity(ab.first(), ab.second());
 		sim_Resim = engine_resim.similarity(ab.first(), ab.second());
 		System.out.println(String.format("sim(a,b): LDSD=%f, Resim=%f", sim_LDSD, sim_Resim));
+		
 		sim_LDSD = engine_ldsd.similarity(ba.first(), ba.second());
 		sim_Resim = engine_resim.similarity(ba.first(), ba.second());
 		System.out.println(String.format("sim(b,a): LDSD=%f, Resim=%f", sim_LDSD, sim_Resim));
 		
 		// Result:
 		// sim(a,a): LDSD=0,845437, Resim=1,000000
+		// sim(b,b): LDSD=0,521786, Resim=1,000000
 		// sim(a,b): LDSD=0,166451, Resim=0,083261
 		// sim(b,a): LDSD=0,166451, Resim=0,083261
-		// => We did not test every possible combination to show that 2) does not always hold true for LDSD.
-		// => However, one can see it from the equation and this is just a single sample from sample data.
+		// => One can see that axiom 1) is clearly violated by LDSD.
+		// => We could not show a violation of 2) here but one can see it in the LDSD equation.
 	}
 }
